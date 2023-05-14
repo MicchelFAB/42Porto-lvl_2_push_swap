0b68f0a4044f8876fcc9d90fd53eaab720f76853
MicchelFAB <micchelfab@gmail.com>
2023-05-14 16:45:49 +0100

    delete

--- .build/base/ft_check_is_int.d
+++
@@ -1,7 +0,0 @@
-.build/base/ft_check_is_int.o: src/base/ft_check_is_int.c \
- src/base/../../inc/push_swap.h src/base/../../inc/libft.h \
- src/base/../../inc/get_next_line.h src/base/../../inc/ft_printf.h
-src/base/../../inc/push_swap.h:
-src/base/../../inc/libft.h:
-src/base/../../inc/get_next_line.h:
-src/base/../../inc/ft_printf.h:
--- .build/base/utils.d
+++
@@ -1,7 +0,0 @@
-.build/base/utils.o: src/base/utils.c src/base/../../inc/push_swap.h \
- src/base/../../inc/libft.h src/base/../../inc/get_next_line.h \
- src/base/../../inc/ft_printf.h
-src/base/../../inc/push_swap.h:
-src/base/../../inc/libft.h:
-src/base/../../inc/get_next_line.h:
-src/base/../../inc/ft_printf.h:
